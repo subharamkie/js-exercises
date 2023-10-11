7391ae26c1e461f257cdbafaf15ff3a857d1a42b
Subha <subharamkie@gmail.com>
2023-10-11 14:54:28 +0100

    feat:duplicatearray() with insertion sort

--- challenges/exercise2.js
+++ challenges/exercise2.js
@@ -16,8 +16,10 @@ export function camelCaseWords(words) {
 	if(words.length === 1){return camelCaseWord;}
 	for(let i =1;i<words.length;i++){
 		//since first word is already initialised, start with index 1
-		camelCaseWord+= words[i][0].toUpperCase()+words[i].slice(1);
+		//camelCaseWord+= words[i][0].toUpperCase() + words[i].slice(1);
+		camelCaseWord+= words[i][0].toUpperCase() + words[i].substring(1);
 	}
+	return camelCaseWord;
 }
 
 export function getTotalSubjects(people) {
@@ -48,9 +50,34 @@ export function checkIngredients(menu, ingredient) {
 	return hasIngredient;
 }
 
-export function duplicateNumbers(arr1, arr2) {
-	if (arr1 === undefined) throw new Error('arr1 is required');
-	if (arr2 === undefined) throw new Error('arr2 is required');
+export function duplicateNumbers(array1, array2) {
+	if (array1 === undefined) throw new Error('arr1 is required');
+	if (array2 === undefined) throw new Error('arr2 is required');
 	// Your code here!
+	let newArray = [];
+	for(let i= 0;i<array1.length;i++){
+		if((array2.indexOf(array1[i])>=0) && (newArray.indexOf(array1[i]) === -1)){
+			newArray = addToArray(newArray,array1[i]);
+		}
+	}
+	return newArray;	
 	
+}
+
+export function addToArray(array,num){
+	if(array === undefined) throw new Error('array is required');
+	if(num === undefined) throw new Error('Number to add is required');
+	if(array.length === 0){ 
+		array.push(num);
+		return array;
+	}
+	for(let i=0;i<array.length;i++){
+		if(num<array[i]){
+			array.splice(i,0,num);
+			return array;
+		}	
+	}
+	array.push(num);
+	return array;	
+
 }
