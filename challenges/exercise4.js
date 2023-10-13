d7392c111150b4e3bc3873df76569c4773ff46c3
Subha <subharamkie@gmail.com>
2023-10-13 09:34:40 +0100

    feat:findNextNumber() implemented

--- challenges/exercise4.js
+++ challenges/exercise4.js
@@ -1,36 +1,47 @@
-export const findNextNumber = (nums, n) => {
-	if (nums === undefined) throw new Error('nums is required');
-	if (n === undefined) throw new Error('n is required');
-	// Your code here!
+export const findNextNumber = (numbers, number) => {
+  if (numbers === undefined) throw new Error("numbers is required");
+  if (number === undefined) throw new Error("number is required");
+  const index = numbers.indexOf(number);
+  return index !== numbers.length - 1 && index !== -1
+    ? numbers[index + 1]
+    : null;
 };
 
-export const count1sand0s = (str) => {
-	if (str === undefined) throw new Error('str is required');
-	// Your code here!
+export const count1sand0s = (binaryStr) => {
+  if (binaryStr === undefined) throw new Error("Binary string is required");
+  const strArray = binaryStr.split('');
+  let binaryCount = {};
+  strArray.forEach((char) => {
+    if (binaryCount.char === undefined) {
+      binaryCount.char = 1;
+    } else {
+      binaryCount.char += 1;
+    }
+  });
+  console.log("obj:"+binaryCount);
+  return binaryCount;
 };
 
 export const reverseNumber = (n) => {
-	if (n === undefined) throw new Error('n is required');
-	// Your code here!
+  if (n === undefined) throw new Error("n is required");
 };
 
 export const sumArrays = (arrs) => {
-	if (arrs === undefined) throw new Error('arrs is required');
-	// Your code here!
+  if (arrs === undefined) throw new Error("arrs is required");
 };
 
 export const arrShift = (arr) => {
-	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+  if (arr === undefined) throw new Error("arr is required");
+  // Your code here!
 };
 
 export const findNeedle = (haystack, searchTerm) => {
-	if (haystack === undefined) throw new Error('haystack is required');
-	if (searchTerm === undefined) throw new Error('searchTerm is required');
-	// Your code here!
+  if (haystack === undefined) throw new Error("haystack is required");
+  if (searchTerm === undefined) throw new Error("searchTerm is required");
+  // Your code here!
 };
 
 export const getWordFrequencies = (str) => {
-	if (str === undefined) throw new Error('str is required');
-	// Your code here!
+  if (str === undefined) throw new Error("str is required");
+  // Your code here!
 };
