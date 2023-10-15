04777984e461917702ab2720d42afa6fffe7885f
Subha <subharamkie@gmail.com>
2023-10-15 13:06:38 +0100

    Feat:Added corner test cases and error messages

--- challenges/exercise7-optional.js
+++ challenges/exercise7-optional.js
@@ -1,26 +1,27 @@
-
 export function printColours(colours) {
-	if (!colours) throw new Error('colour is required');
-	return [...colours];
+  if (colours === undefined || colours.length === 0)
+    throw new Error("Colour array is required");
+  return [...colours];
 }
 
 export function shoppingList(list, newItem) {
-	if (!list) throw new Error('list is required');
-	if (!newItem) throw new Error('newItem is required');
-	return [...list,...newItem];
+  if (list === undefined) throw new Error("List is required");
+  if (newItem === undefined) throw new Error("New item is required");
+  return [...list, ...newItem];
 }
 
 export function highestNumber(numbers) {
-	if (!numbers) throw new Error('numbers are required');
-	return Math.max(...numbers);
+  if (numbers === undefined || numbers.length === 0)
+    throw new Error("Numbers array is required");
+  return Math.max(...numbers);
 }
 
 export function splitThatString(string) {
-	if (!string) throw new Error('string is required');
-	return string.split('');
+  if (!string || string === "") throw new Error("String is required");
+  return string.split("");
 }
 
 export function addressLookUp(user) {
-	if (!user) throw new Error('user is required');
-	return user?.address?.postcode;
+  if (!user) throw new Error("User is required");
+  return user?.address?.postcode;
 }
--- test/exercise7-optional.test.js
+++ test/exercise7-optional.test.js
@@ -1,84 +1,114 @@
 import {
-	printColours,
-	shoppingList,
-	highestNumber,
-	splitThatString,
-	addressLookUp,
-} from '../challenges/exercise7-optional';
+  printColours,
+  shoppingList,
+  highestNumber,
+  splitThatString,
+  addressLookUp,
+} from "../challenges/exercise7-optional";
 
-describe('colours', () => {
-	test('returns a list of colours', () => {
-		const colours = ['red', 'orange', 'green'];
-		expect(printColours(colours)).toEqual(colours);
-	});
+describe("colours", () => {
+  test("returns a list of colours", () => {
+    const colours = ["red", "orange", "green"];
+    expect(printColours(colours)).toEqual(colours);
+  });
+  test("Error if array is not passed or empty", () => {
+    expect(() => {
+      printColours();
+    }).toThrow("Colour array is required");
+    expect(() => {
+      printColours([]);
+    }).toThrow("Colour array is required");
+  });
 });
 
-describe('shoppingList', () => {
-	test('returns a list with an extra item', () => {
-		const list = [
-			'one apple',
-			'two dozen organic eggs',
-			'cucumber',
-			'bread',
-		];
-		const newItem = ['earl grey tea'];
-		expect(shoppingList(list, newItem)).toEqual([
-			'one apple',
-			'two dozen organic eggs',
-			'cucumber',
-			'bread',
-			'earl grey tea',
-		]);
-	});
+describe("shoppingList", () => {
+  test("returns a list with an extra item", () => {
+    const list = ["one apple", "two dozen organic eggs", "cucumber", "bread"];
+    const newItem = ["earl grey tea"];
+    expect(shoppingList(list, newItem)).toEqual([
+      "one apple",
+      "two dozen organic eggs",
+      "cucumber",
+      "bread",
+      "earl grey tea",
+    ]);
+  });
 
-	test('returns an empty list with new items added to it', () => {
-		const list = [];
-		const newItem = ['jelly', 'melon', 'pear'];
-		expect(shoppingList(list, newItem)).toEqual(['jelly', 'melon', 'pear']);
-	});
+  test("returns an empty list with new items added to it", () => {
+    const list = [];
+    const newItem = ["jelly", "melon", "pear"];
+    expect(shoppingList(list, newItem)).toEqual(["jelly", "melon", "pear"]);
+  });
+  test("Error if array is not passed", () => {
+    expect(() => {
+      shoppingList();
+    }).toThrow("List is required");
+  });
+  test("Error if new item to add is not passed", () => {
+    expect(() => {
+      shoppingList([]);
+    }).toThrow("New item is required");
+  });
 });
 
-describe('highestNumber', () => {
-	test('returns the highest number', () => {
-		const numbers = [12, 20, 18];
-		expect(highestNumber(numbers)).toEqual(20);
-	});
+describe("highestNumber", () => {
+  test("returns the highest number", () => {
+    const numbers = [12, 20, 18];
+    expect(highestNumber(numbers)).toEqual(20);
+  });
+  test("Error if array is not passed", () => {
+    expect(() => highestNumber()).toThrow("Numbers array is required");
+  });
+  test("Error if array is empty", () => {
+    expect(() => highestNumber([])).toThrow("Numbers array is required");
+  });
 });
 
-describe('splitThatString', () => {
-	test('returns a string split up', () => {
-		const string = 'USA';
-		expect(splitThatString(string)).toEqual(['U', 'S', 'A']);
-	});
-	test('returns a string split up', () => {
-		const string = '123';
-		expect(splitThatString(string)).toEqual(['1', '2', '3']);
-	});
+describe("splitThatString", () => {
+  test("returns a string split up", () => {
+    const string = "USA";
+    expect(splitThatString(string)).toEqual(["U", "S", "A"]);
+  });
+  test("returns a string split up", () => {
+    const string = "123";
+    expect(splitThatString(string)).toEqual(["1", "2", "3"]);
+  });
+  test("Error if string is not passed", () => {
+    expect(() => splitThatString()).toThrow("String is required");
+  });
+  test("Error if string is empty", () => {
+    expect(() => splitThatString("")).toThrow("String is required");
+  });
 });
 
 // Optional Chaining
 
-describe('addressLookUp', () => {
-	test('returns a users postcode', () => {
-		const user = {
-			name: 'JimBob Eggs',
-			telephone: 666,
-			likes: 'cheese',
-			dislikes: 'dirty socks',
-			address: {
-				houseNumber: 'The Number of the Beast',
-				postcode: 'H12 333',
-			},
-		};
-		expect(addressLookUp(user)).toEqual('H12 333');
-	});
-	test("returns undefined if address isn't provided", () => {
-		const user = {
-			name: 'JimBob Eggs',
-			telephone: 666,
-			likes: 'cheese',
-			dislikes: 'dirty socks',
-		};
-		expect(addressLookUp(user)).toEqual(undefined);
-	});
+describe("addressLookUp", () => {
+  test("returns a users postcode", () => {
+    const user = {
+      name: "JimBob Eggs",
+      telephone: 666,
+      likes: "cheese",
+      dislikes: "dirty socks",
+      address: {
+        houseNumber: "The Number of the Beast",
+        postcode: "H12 333",
+      },
+    };
+    expect(addressLookUp(user)).toEqual("H12 333");
+  });
+  test("returns undefined if address isn't provided", () => {
+    const user = {
+      name: "JimBob Eggs",
+      telephone: 666,
+      likes: "cheese",
+      dislikes: "dirty socks",
+    };
+    expect(addressLookUp(user)).toEqual(undefined);
+  });
+  test("Error if user object is not passed", () => {
+    expect(() => {
+      addressLookUp();
+    }).toThrow("User is required");
+  });
 });
